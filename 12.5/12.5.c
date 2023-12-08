cca3bbc3a1447e43e2a37b48440d5738a8a2d517
Spade9 <2117970085@qq.com>
2023-12-08 08:51:50 +0800

    improve code 12.5

--- 12.5/12.5.c
+++ 12.5/12.5.c
@@ -14,20 +14,16 @@ int main()
             sum[i] += matrix[i][j];
         }
     }
-    int sum0;
     for (int i = 0; i < n - 1; i++)
     {
-        sum0 = sum[mark[0]];
         for (int j = 0; j < n - 1 - i; j++)
         {
-            if (sum0 > sum[mark[j + 1]])
+            if (sum[mark[j]] > sum[mark[j + 1]])
             {
                 int tmp = mark[j];
                 mark[j] = mark[j + 1];
                 mark[j + 1] = tmp;
             }
-            else
-                sum0 = sum[mark[j + 1]];
         }
     }
     for (int i = 0; i < n; i++)
@@ -36,6 +32,5 @@ int main()
             printf("%d ", matrix[mark[i]][j]);
         printf("%d\n", matrix[mark[i]][n - 1]);
     }
-    printf(" ");
     return 0;
 }
